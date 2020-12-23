7072b415d90032d552507bf6014e0f4387a29407
MoritzPatek <57459930+MoritzPatek@users.noreply.github.com>
2020-12-23 21:09:08 +0100

    changes on the map and movement script

---
+++ Assets/Scripts/carMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carMovement : MonoBehaviour
+{
+    [SerializeField] private float movingSpeed;
+    private GameObject car;
+    private Rigidbody rb;
+
+    Vector3 rotationRight = new Vector3(0, 30, 0);
+    Vector3 rotationLeft = new Vector3(0, -30, 0);
+
+    Vector3 forward = new Vector3(0, 0, 1);
+    
+    //this value multiplies with the movespeed
+    public float moveInput;
+
+    //when this value is under 0.4 its turning right 
+    //when its over 0.6 then its turning left
+    //the 0.2 inbetween are there so the ai can drive straight 
+    public float rotateInput;
+    
+    void Start()
+    {
+        car = this.gameObject;
+        rb = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(forward * (movingSpeed * moveInput * Time.deltaTime));
+        
+        if (rotateInput < 0.4f) {
+            Quaternion deltaRotationRight = Quaternion.Euler(rotationRight * Time.deltaTime);
+            rb.MoveRotation(rb.rotation * deltaRotationRight);
+        }
+        if(rotateInput > 0.6f){
+            Quaternion deltaRotationLeft = Quaternion.Euler(rotationLeft * Time.deltaTime);
+            rb.MoveRotation(rb.rotation * deltaRotationLeft); 
+        }
+        
+    }
+}
