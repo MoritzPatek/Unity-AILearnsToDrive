22a169051630397017c92fe0e0084211cf063a8a
MoritzPatek <57459930+MoritzPatek@users.noreply.github.com>
2020-12-24 16:10:12 +0100

    neural network changes

---
+++ Assets/Scripts/colliderController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colliderController : MonoBehaviour
+{
+    private brain _brain;
+    private carMovement _carMovement;
+
+    private void Start()
+    {
+        _brain = gameObject.GetComponent<brain>();
+        _carMovement = gameObject.GetComponent<carMovement>();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        _carMovement.Respawn();
+        _brain.CarCrash();
+    }
+}
