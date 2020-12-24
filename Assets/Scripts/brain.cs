79871e650d141eaa2444fc35433a881a185634a9
MoritzPatek <57459930+MoritzPatek@users.noreply.github.com>
2020-12-24 14:37:30 +0100

    init biases and neurons

--- Assets/Scripts/brain.cs
+++ Assets/Scripts/brain.cs
@@ -11,35 +11,56 @@ public class brain : MonoBehaviour
     //those are the values i will use to tell the neural network, if it messed up or if it performed good
     public float distanceWent; //fitness
 
-    private int[] layers;
-    private float[][] neurons;
+    private int[] layers = new int[] {7,5,2}; 
+
+    private float[][][] weights; 
     private float[][] biases;
-    private float[][][] weights;
-    private int[] activations;
+    private float[][] neurons;
+    private float[] activations; 
+    
+    
 
 
     private void Start()
     {
         _sensors = this.gameObject.GetComponent<sensors>();
         _carMovement = this.gameObject.GetComponent<carMovement>();
-
+        InitNeurons();
         InitWeights();
         InitBiases();
-        InitNeurons();
+        
     }
 
+    void InitNeurons()
+    {
+        List<float[]> neuronsList = new List<float[]>();
+        for (int i = 0; i < layers.Length; i++)
+        {
+            neuronsList.Add(new float[layers[i]]);
+        }
+        neurons = neuronsList.ToArray();
+    }
+    
     void InitWeights()
     {
         
     }
     void InitBiases()
     {
-        
+        List<float[]> biasList = new List<float[]>();
+
+        for (int i = 0; i < layers.Length; i++)
+        {
+            float[] bias = new float[layers[i]];
+            for (int x = 0; x < bias.Length; x++)
+            {
+                bias[x] = UnityEngine.Random.Range(-0.5f, 0.5f);
+            }
+            biasList.Add(bias);
+        }
+        biases = biasList.ToArray();
     }
-    void InitNeurons()
-    {
-        
-    }
+    
 
 
 
@@ -47,12 +68,21 @@ public class brain : MonoBehaviour
     {
         //creating a list of all the sensor data we have 
         List<float> originInputs = new List<float>() {_sensors.currentSpeed, _sensors.rotation, _sensors.distanceForward, _sensors.distanceLeft, _sensors.distanceRight, _sensors.distanceFLeft, _sensors.distanceFRight};
-        /*string text = null; 
-        foreach (var input in inputs)
-        {
-            text = text + input.ToString() + " ";
-        }
-        Debug.Log(text);*/
+        List<float> output = feedForward(originInputs);
+        _carMovement.moveInput = output[0];
+        _carMovement.rotateInput = output[1];
+    }
+
+    private List<float> feedForward(List<float> inputs)
+    {
+        List<float> outputs = new List<float>() {1,1};
+        return outputs;
+    }
+
+    private float sigmoidFunction(float value)
+    {
+        float k = Mathf.Exp(value);
+        return k / (1.0f + k);
     }
 
     public void CarCrash()
